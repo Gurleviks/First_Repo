b1d3ac39804e72490aa1b793751f0685db5a3331
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2022-12-23 12:35:34 +0300

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,6 @@
+##Create two variables called name and age.
+##Then create a variable called hello and print the name and the age.
+##Use the formatted string literal.
+name = 'Wololo'
+age = 58
+print(f"Hello, My name is {name} and I'm {age} years old.")
