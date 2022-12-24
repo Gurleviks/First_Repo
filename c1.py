b2bdbf58d347643d03d88a248162768e88f7afb7
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2022-12-24 18:33:45 +0300

    Update c1.py

--- c1.py
+++ c1.py
@@ -1,4 +1,7 @@
-num1=int(input("335"))
-num2=int(input("66 "))
-if(num1>num2): print(num1)
-elif(num2>num1): print(num2)
+num1 = 335
+num2 = 66
+
+if (num1>num2):
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num1} is not greater than {num2}")
