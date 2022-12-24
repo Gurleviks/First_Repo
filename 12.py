be68c4cdde580ae04464e9bd0b56cc3281bb1ba4
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2022-12-24 18:58:42 +0300

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,6 +1,4 @@
-##Create two variables called name and age.
-##Then create a variable called hello and print the name and the age.
-##Use the formatted string literal.
 name = 'Wololo'
 age = 58
-print(f"Hello, My name is {name} and I'm {age} years old.")
+hello = 'Hello'
+print(hello, name, age)
